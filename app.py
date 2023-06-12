7f1a60f4a9af53521e428a6d4311109e965df769
Jimson James <jimson.james@intellitix.com>
2023-06-12 14:39:26 -0400

    updated docstrings

--- app.py
+++ app.py
@@ -15,10 +15,26 @@ from enum import Enum
 
 
 class Player(Enum):
+    """
+        Represents a player in a game.
+        Each player is assigned a unique identifier and is represented as a string.
+
+        Raises:
+            ValueError: Raies when the player identifier is not valid value
+
+        Returns:
+            Player: The player.
+    """
     ONE = 1
     TWO = 2
 
     def __str__(self):
+        """
+        Returns the string representation of the player.
+
+        Returns:
+            str: The string representation of the player.
+        """
         if self == Player.ONE:
             return "Player-ONE"
         elif self == Player.TWO:
@@ -26,6 +42,18 @@ class Player(Enum):
 
     @staticmethod
     def to_string(value):
+        """
+        Converts the given player's numeric value to its corresponding string representation.
+
+        Args:
+            value (int): The value of the player.
+
+        Returns:
+            str: The string representation of the player.
+
+        Raises:
+            ValueError: If the value is invalid.
+        """
         for member in Player:
             if member.value == value:
                 return str(member)
@@ -33,7 +61,19 @@ class Player(Enum):
 
 
 class TennisScoringBoard:
+
+    """
+    Represents the tennis scoring board for tracking the progress of a tennis match.
+    """
+
     def __init__(self, player1_name, player2_name):
+        """
+        Initializes the TennisScoringBoard with the names of the two players.
+
+        Args:
+            player1_name (str): The name of player one.
+            player2_name (str): The name of player two.
+        """
         self.player1_name = player1_name
         self.player2_name = player2_name
         self.player1_points = 0
@@ -45,10 +85,18 @@ class TennisScoringBoard:
         self.game_over = False
 
     def update_score(self, point_winner):
+        """
+        Updates the score based on the winner of a point.
+
+        Args:
+            point_winner (int): The value representing the winner of the point (1 or 2).
+        """
+
         if point_winner == 1:
             self.player1_points += 1
         else:
             self.player2_points += 1
+
         # REQ-1 Player with 4 points wins the game
         # REQ-2 Player needs to win by 2 points.
         if self.player1_points >= 4 and self.player1_points - self.player2_points >= 2:
@@ -70,14 +118,23 @@ class TennisScoringBoard:
             self.game_over = True
 
     def reset_points(self):
+        """
+        Resets the points for both players to zero.
+        """
         self.player1_points = 0
         self.player2_points = 0
 
     def reset_games(self):
+        """
+        Resets the games for both players to zero.
+        """
         self.player1_games = 0
         self.player2_games = 0
 
     def display_score(self):
+        """
+        Displays the current score board.
+        """
         print("--------------")
         print("Score:")
         print(f"{self.player1_name}: Sets - {self.player1_sets} | Games - {self.player1_games} | Points - {self.points_to_string(self.player1_points)}")
@@ -86,6 +143,15 @@ class TennisScoringBoard:
 
     @staticmethod
     def points_to_string(points):
+        """
+        Converts the given points to their corresponding string representation.
+
+        Args:
+            points (int): The points to be converted.
+
+        Returns:
+            str: The string representation of the points.
+        """
         if points == 0:
             return "0"
         elif points == 1:
@@ -100,7 +166,7 @@ class TennisScoringBoard:
             return ""
 
 
-# Example usage:
+# Example usage: Start with two player, assuming singles; later onto doubles.
 player1_name = "Player One"
 player2_name = "Player Two"
 scoring_board = TennisScoringBoard(player1_name, player2_name)
@@ -114,6 +180,7 @@ while not scoring_board.game_over:
 
 print("--Game finished--")
 
+# Player won 2/3 sets.
 # Celebrate the winner
 if scoring_board.player1_sets == 2:
     print(f"{player1_name} wins the match!")
